858bc6a760ecb8319a933a14d9310077532ee09e
OscarTadeo <42451057+OscarTadeo@users.noreply.github.com>
2021-08-13 19:02:26 -0500

    contador funcional
    Programa que incrementa y resetea al textView

--- Contador/app/src/main/java/com/example/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //Comentario
+    private TextView numeros;
+    private Button btn_i, btn_r;
+    int n = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializar();
     }
+
+    private void inicializar() {
+        numeros = (TextView)findViewById(R.id.txt_contador);
+        btn_i = findViewById(R.id.button);
+        btn_r = findViewById(R.id.button2);
+
+        btn_i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incrementar();
+                //Log.i("n: ",String.valueOf(n));
+            }
+        });
+
+        btn_r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+    }
+
+    private void incrementar(){
+        ++n;
+        numeros.setText(Integer.toString(n));
+    }
+
+    private void reset(){
+        n=0;
+        numeros.setText(Integer.toString(0));
+    }
+
 }
